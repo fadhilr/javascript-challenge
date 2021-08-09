895d0dff7e6b885acefbb5f9e115684d6c64226d
fadhilr <fadilrizqullah97@gmail.com>
2021-08-09 22:38:06 +0700

    Commit soal CaseStudy1 dan CaseStudy2

--- CaseStudy1.js
+++ CaseStudy1.js
@@ -1,4 +1,19 @@
 var x = 5;
 var y = 8.5;
 
-//buatlah proses perhitungan penjumlahan, pengurangan, perkalian, dan pembagian dari kedua variabel di atas.
+//buatlah proses perhitungan penjumlahan, pengurangan, perkalian, dan pembagian dari kedua variabel di atas.\
+// penjumlahan
+var jumlah = x+y;
+document.write('Hasil jumlah = '+jumlah+'<br>')
+
+//pengurangan
+var kurang = x-y;
+document.write('Hasil kurang = '+kurang+'<br>')
+
+//perkalian
+var kali = x*y;
+document.write('Hasil kali = '+kali+'<br>')
+
+//pembagian
+var bagi = x/y;
+document.write('Hasil bagi = '+bagi+'<br>')
--- CaseStudy2.js
+++ CaseStudy2.js
@@ -1,4 +1,4 @@
-function nilaiPeserta (nilai) {
+function nilaiPeserta(nilai) {
     /* 
         Buat proses seleksi kondisi untuk melakukan klasifikasi nilai peserta dengan rincian:
         A = 80 - 100
@@ -7,7 +7,30 @@ function nilaiPeserta (nilai) {
         D = 20 - 40
         E = >20
     */
+    switch (true) {
+        case nilai >= 80:
+            if (nilai>100) {
+                document.write('Nilai melewati score maksimal'+'<br>')
+            } else {
+                document.write('Nilai anda  = A'+'<br>')
+            }
+            break;
+        case nilai >= 60:
+            document.write('Nilai anda  = B'+'<br>')
+            break;
+        case nilai >= 40:
+            document.write('Nilai anda  = C'+'<br>')
+            break;
+        case nilai >= 20:
+            document.write('Nilai anda  = D'+'<br>')
+            break;
+        default:
+            document.write('Nilai anda  = E'+'<br>')
+            break;
+    }
 }
+var nilai = parseInt(prompt('Masukkan nilai siswa = '));
+nilaiPeserta(nilai);
 
 /*
     Silahkan panggil fungsi nilaiPeserta dengan parameter beberapa nilai: 
