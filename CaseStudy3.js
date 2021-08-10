8cde9de3e981653d907e85de36d02132843afa92
fadhilr <fadilrizqullah97@gmail.com>
2021-08-10 18:31:14 +0700

    Commit caseStudy3

--- CaseStudy3.js
+++ CaseStudy3.js
@@ -1,19 +1,37 @@
-function nilaiPeserta (nilai) {
-    /* 
-        Buat proses seleksi kondisi untuk melakukan klasifikasi nilai peserta dengan rincian:
-        A = 80 - 100
-        B = 60 - 80
-        C = 40 - 60
-        D = 20 - 40
-        E = >20
-    */
+function nilaiPeserta(nilai) {
+    switch (true) {
+        case nilai >= 80:
+            if (nilai > 100) {
+                return 'Nilai melebihi maksimum score'
+            } else {
+                return 'A'
+            }
+            break;
+        case nilai >= 60:
+            return 'B'
+            break;
+        case nilai >= 40:
+            return 'C'
+            break;
+        case nilai >= 20:
+            return 'D'
+            break;
+        default:
+            return 'E'
+            break;
+    }
 }
-
+var nilai = 22;
 var dataSiswa = [
-    ["Jojo", 55.5, nilaiPeserta(nilai)],
-    ["Andika", 70.1, nilaiPeserta(nilai)],
-    ["Alif", 19, nilaiPeserta(nilai)],
-    ["Vista", 102, nilaiPeserta(nilai)]
+    ["Jojo", 55.5],
+    ["Andika", 70.1],
+    ["Alif", 19],
+    ["Vista", 102]
 ];
-
 //Silahkan gunakan proses perulangan untuk mengambil semua data array dataSiswa dan tampilkan outputnya.
+for (let index = 0; index < dataSiswa.length; index++) {
+    var nilai = dataSiswa[index][1];
+    var grade = nilaiPeserta(nilai);
+    var nama = dataSiswa[index][0];
+    document.write('Nilai yang didapatkan kak ' + nama + ' yaitu = ' + nilai + '. Dengan grade ' + grade + '<br>');
+}
