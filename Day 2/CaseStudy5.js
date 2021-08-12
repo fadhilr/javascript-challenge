a21aa20af78da6fde710ebc86d67b2e7d03d5560
fadhilr <fadilrizqullah97@gmail.com>
2021-08-12 18:39:37 +0700

    Commit caseStudy 5

--- Day 2/CaseStudy5.js
+++ Day 2/CaseStudy5.js
@@ -6,3 +6,62 @@ yang ketika halaman diload akan menjalankan urutan program sebagai berikut :
 3, masing masing menu yang dipilih akan memberikan parameter berbeda-beda sesuai dengan kebutuhan masing-masing menu
 4. tampilkan hasil perhitunganya di document html
 */
+
+function viewLuas(){
+    document.getElementById('viewLuas').style.display='block';
+}
+
+function viewKeliling(){
+    document.getElementById('viewKeliling').style.display='block';
+}
+
+function segiEmpatL() {
+    var sisi = parseInt(prompt('Masukkan sisi segi empat = '))
+    var hasil = sisi * sisi
+    document.getElementById('hasilLuas').innerHTML= 'Hasil nya adalah = '+hasil
+}
+
+function segiPanjangL() {
+    var panjang = parseInt(prompt('Masukkan panjang segi panjang = '))
+    var lebar = parseInt(prompt('Masukkan lebar segi panjang = '))
+    var hasil = panjang * lebar
+    document.getElementById('hasilLuas').innerHTML= 'Hasil nya adalah = '+hasil
+}
+
+function segiTigaL() {
+    var alas = parseInt(prompt('Masukkan nilai alas = '))
+    var tinggi = parseInt(prompt('Masukkan nilai tinggi = '))
+    var hasil = 1/2*alas*tinggi
+    document.getElementById('hasilLuas').innerHTML= 'Hasil nya adalah = '+hasil
+}
+
+function lingkaranL() {
+    var jariJari = parseInt(prompt('Masukkan jari-jari lingkaran = '))
+    var hasil = (22/7) * jariJari *jariJari
+    document.getElementById('hasilLuas').innerHTML= 'Hasil nya adalah = '+hasil
+}
+
+function segiEmpatK() {
+    var sisi = parseInt(prompt('Masukkan panjang segi empat = '))
+    var hasil = 4 * sisi
+    document.getElementById('hasilKeliling').innerHTML= 'Hasil nya adalah = '+hasil
+}
+
+function segiPanjangK() {
+    var panjang = parseInt(prompt('Masukkan panjang segi panjang = '))
+    var lebar = parseInt(prompt('Masukkan lebar segi panjang = '))
+    var hasil = (2*panjang) + (2*lebar)
+    document.getElementById('hasilKeliling').innerHTML= 'Hasil nya adalah = '+hasil
+}
+
+function segiTigaK() {
+    var sisi = parseInt(prompt('Masukkan nilai sisi segitiga = '))
+    var hasil = 3*sisi
+    document.getElementById('hasilKeliling').innerHTML= 'Hasil nya adalah = '+hasil
+}
+
+function lingkaranK() {
+    var jariJari = parseInt(prompt('Masukkan jari-jari lingkaran = '))
+    var hasil = 2 * (22/7) * jariJari
+    document.getElementById('hasilKeliling').innerHTML= 'Hasil nya adalah = '+hasil
+}
